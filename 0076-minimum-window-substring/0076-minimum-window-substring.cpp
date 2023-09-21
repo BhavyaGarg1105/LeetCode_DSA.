555839438d3ff47d93204faf33fe96333afe4f17
Bhavya Garg <60601159+BhavyaGarg1105@users.noreply.github.com>
2023-09-21 15:52:36 +0530

    Time: 19 ms (56.84%), Space: 8.3 MB (22.57%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,37 +2,36 @@ class Solution {
 public:
     string minWindow(string s, string t) 
     {
-        vector<string> ans;
         map<char,int> mp;
-
         for(auto i: t)
         {
             mp[i]++;
         }
 
-        int start=0;
-        int end=0;
+        int start=0, end=0;
         int count = mp.size();
         int st=0;
         int mini=INT_MAX;
 
-        while(end<s.size()){
+        while(end<s.size())
+        {
             mp[s[end]]--;
-
-            if(mp[s[end]]==0){
+            if(mp[s[end]]==0)
+            {
                 count--;
             }
-
-            if(count==0){
-                while(count==0){
-                   if((end-start+1)<mini){
+            if(count==0)
+            {
+                while(count==0)
+                {
+                   if((end-start+1)<mini)
+                   {
                        mini=end-start+1;
                        st=start;
                    }
-
                    mp[s[start]]++;
-
-                   if(mp[s[start]]>0){
+                   if(mp[s[start]]>0)
+                   {
                        count++;
                    }
                    start++;
@@ -40,11 +39,13 @@ public:
             }
             end++;
         }
-      
-        if(mini==INT_MAX){
+
+        if(mini==INT_MAX)
+        {
             return "";
         }
-        else{
+        else
+        {
             return s.substr(st,mini);
         }
     }
