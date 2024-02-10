e6124fcede0911691955a7968889e00198a8cf82
Bhavya Garg <60601159+BhavyaGarg1105@users.noreply.github.com>
2024-02-10 20:12:12 +0530

    Time: 4 ms (38.41%) | Memory: 7.5 MB (33.07%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -3,16 +3,14 @@ public:
     int lengthOfLastWord(string s) {
         int n = s.length();
         int i = n - 1, ans = 0;
-        if(s[i]==' '){
-        while(s[i]==' ' && i!=0)
-        i--;
-        }
+        
+            while(i>=0 && s[i]==' ' ){
+            i--;
+            }
 
-        while(s[i]!=' '){
+        while( i>=0 && s[i]!=' '){
             ans++;
             i--;
-            if(i<0)
-            break;
         }
         return ans;
     }
